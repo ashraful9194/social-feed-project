d69bfe1e751903dcadbcea81ea68719b6034a296
Ashraful Alam <ashraful9194@gmail.com>
2025-11-25 10:53:57 +0600

    Docker file location changed

--- SocialFeed.API/DTOs/FeedResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace SocialFeed.API.DTOs;
-
-public class FeedResponse
-{
-    public List<PostResponse> Posts { get; set; } = new();
-    public DateTime? NextCursorCreatedAt { get; set; }
-    public int? NextCursorId { get; set; }
-    public bool HasMore { get; set; }
-}
